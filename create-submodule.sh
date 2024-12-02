ecee7c9233e039f4dd7809a5ce1686ad1f763f02
InCodeo <nickbuckjones@gmail.com>
2024-12-02 22:50:39 +1100

    Update create-submodule.sh

--- create-submodule.sh
+++ create-submodule.sh
@@ -13,8 +13,11 @@ REPO_NAME="day-$DAY_NUM"
 echo "Creating repository $REPO_NAME..."
 gh repo create "$REPO_NAME" --private --confirm
 
-# Add as submodule
+# Get the user's GitHub username
+GITHUB_USER=$(gh api user -q .login)
+
+# Add as submodule using HTTPS
 echo "Adding as submodule..."
-git submodule add "git@github.com:$(gh api user -q .login)/$REPO_NAME.git" "$REPO_NAME"
+git submodule add "https://github.com/$GITHUB_USER/$REPO_NAME.git" "$REPO_NAME"
 
 echo "Successfully created and added $REPO_NAME as a submodule"
